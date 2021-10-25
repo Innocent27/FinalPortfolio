84149ae726e0b6bbee0fe1812aebbc320383258d
Innocent <lindokuhlenkamela@gmail.com>
2021-10-25 15:27:03 +0200

    landing page profile added

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+  document.getElementById("nav-links").classList.toggle("active");
+}
+
+function scrollToSection(elementID) {
+  toggleNav();
+  document.getElementById(elementID).scrollIntoView();
+}
