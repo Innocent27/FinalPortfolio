07f63c760e86582eee17da584e5795d0059af30f
Innocent <lindokuhlenkamela@gmail.com>
2021-10-29 09:35:58 +0200

    Updated footer section

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    document.getElementById("nav-links").classList.toggle("active");
-  }
-  
-  function scrollToSection(elementID) {
-    toggleNav();
-    document.getElementById(elementID).scrollIntoView();
-  }
